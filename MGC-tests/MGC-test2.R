8c826d03cdfc99741f17df5e76fd168d3a42cd83
pmyers16 <pmyers16@jhu.edu>
2018-12-03 20:03:11 -0500

    MGC run on all features

--- MGC-tests/MGC-test2.R
+++ MGC-tests/MGC-test2.R
@@ -38,24 +38,26 @@ plot_mtx <- function(Dx, main.title="Local Correlation Map", xlab.title="# X Nei
 set.seed(12345)
 dat <- read.csv("hbn_vertexstats.csv", header = TRUE)
 #dat <- array(as.numeric(unlist(dat)), dim=c(91,291))
-Y <- dat[,2]
-print(Y)
-X1 <- dat[,c(4:51)]
-X2 <- dat[,c(52:99)]
-X3 <- dat[,c(100:147)]
-X4 <- dat[,c(148:195)]
-X5 <- dat[,c(196:243)]
-X6 <- dat[,c(244:291)]
+Y1 <- dat[,2]
+Y2 <- dat[,3]
+print(Y1)
+#X1 <- dat[,c(4:51)]
+#X2 <- dat[,c(52:99)]
+#X3 <- dat[,c(100:147)]
+#X4 <- dat[,c(148:195)]
+#X5 <- dat[,c(196:243)]
+#X6 <- dat[,c(244:291)]
 
-zeromat <- matrix(1:6, nrow = 6, ncol=6)
+zeromat <- matrix(1:288, nrow = 288, ncol=6)
 mgctests <- array(0L, dim(zeromat))
 
 count <- 1
 
-Xlist <- list(X1, X2, X3, X4, X5, X6)
-for(i in Xlist){
-  Xdat <- array(as.numeric(unlist(i)), dim=c(91,48))
-  YDat <- array(as.numeric(unlist(Y)), dim=c(91,1))
+#Xlist <- list(X1, X2, X3, X4, X5, X6)
+for(i in 4:291){
+  X <- dat[,i]
+  Xdat <- array(as.numeric(unlist(X)), dim=c(91,1))
+  YDat <- array(as.numeric(unlist(Y1)), dim=c(91,1))
   res <- mgc.test(Xdat,YDat, rep=20)
   val <- res$statMGC
   p_val <- res$pMGC
@@ -70,10 +72,39 @@ for(i in Xlist){
   count <- count +1
 }
 
-Xdat <- array(as.numeric(unlist(X1)), dim=c(91,48))
-YDat <- array(as.numeric(unlist(Y)), dim=c(91,1))
-res <- mgc.test(Xdat,YDat, rep=20)
-corr_mat <- res$localCorr
+#Xdat <- array(as.numeric(unlist(X1)), dim=c(91,48))
+#YDat <- array(as.numeric(unlist(Y)), dim=c(91,1))
+#res <- mgc.test(Xdat,YDat, rep=20)
+#corr_mat <- res$localCorr
 
-write.csv(mgctests, file = 'mgc-test-stats_vertex.csv')
-write.csv(corr_mat, file = 'Example_vertex_corr.csv')
+write.csv(mgctests, file = 'mgc-test-stats_gender.csv')
+
+
+
+zeromat <- matrix(1:288, nrow = 288, ncol=6)
+mgctests <- array(0L, dim(zeromat))
+
+count <- 1
+
+#Xlist <- list(X1, X2, X3, X4, X5, X6)
+for(i in 4:291){
+  X <- dat[,i]
+  Xdat <- array(as.numeric(unlist(X)), dim=c(91,1))
+  YDat <- array(as.numeric(unlist(Y2)), dim=c(91,1))
+  res <- mgc.test(Xdat,YDat, rep=20)
+  val <- res$statMGC
+  p_val <- res$pMGC
+  scale <- res$optimalScale
+  
+  mgctests[count,1] = val
+  mgctests[count,2] = 'Xdat'
+  mgctests[count,3] = 'YDat'
+  mgctests[count,4] = p_val
+  mgctests[count,5] = scale$x
+  mgctests[count,6] = scale$y
+  count <- count +1
+}
+
+write.csv(mgctests, file = 'mgc-test-stats_age.csv')
+
+#write.csv(corr_mat, file = 'Example_vertex_corr.csv')
